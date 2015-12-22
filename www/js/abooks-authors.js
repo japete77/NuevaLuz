4aef74b5ca752ad2c609381688b99fb97091251b
japete <japete.gonzalez@gmail.com>
2015-12-22 23:12:58 +0100

    Search filter added

--- www/js/abooks-authors.js
+++ www/js/abooks-authors.js
@@ -1,12 +1,14 @@
 // Authors
-app.controller('AuthorsCtrl', ['$scope', '$http', '$ionicLoading', '$location', 'SvcNL', 
-function($scope, $http, $ionicLoading, $location, SvcNL) {
+app.controller('AuthorsCtrl', ['$scope', '$http', '$ionicLoading', '$ionicScrollDelegate', '$location', 'SvcNL', 
+function($scope, $http, $ionicLoading, $ionicScrollDelegate, $location, SvcNL) {
 	var index = 1;
 	var maxAuthors = 9999999;
 	var pageSize = 15;
 	var requesting = false;
 	
 	$scope.authors = [];
+    
+    $scope.filterText = "";
 
 	$scope.GetNextAuthors = function() {
 	
@@ -14,22 +16,43 @@ function($scope, $http, $ionicLoading, $location, SvcNL) {
 			
 			requesting = true;
 			
-			$http({
-				method: 'GET',
-				url: baseUrl + 'GetAuthors?Session=' + SvcNL.GetSession() + '&Index=' + index + '&Count=' + pageSize
-			})
-			.then(function success(response) {
-			
-				maxAuthors = response.data.GetAuthorsResult.Total;
-				
-				response.data.GetAuthorsResult.Authors.forEach(function(element) {
-					$scope.authors.push(element);
-				}, this);
-				
-				index += pageSize;
-				
-				requesting = false;
-			})
+            if ($scope.filterText=="") {
+                $http({
+                    method: 'GET',
+                    url: baseUrl + 'GetAuthors?Session=' + SvcNL.GetSession() + '&Index=' + index + '&Count=' + pageSize
+                })
+                .then(function success(response) {
+                
+                    maxAuthors = response.data.GetAuthorsResult.Total;
+                    
+                    response.data.GetAuthorsResult.Authors.forEach(function(element) {
+                        $scope.authors.push(element);
+                    }, this);
+                    
+                    index += pageSize;
+                    
+                    requesting = false;
+                })
+            }
+            else {
+                $http({
+                    method: 'GET',
+                    url: baseUrl + 'SearchAuthors?Session=' + SvcNL.GetSession() + '&Text=' + $scope.filterText + '&Index=' + index + '&Count=' + pageSize
+                })
+                .then(function success(response) {
+                
+                    maxAuthors = response.data.SearchAuthorsResult.Total;
+                    
+                    response.data.SearchAuthorsResult.Authors.forEach(function(element) {
+                        $scope.authors.push(element);
+                    }, this);
+                    
+                    index += pageSize;
+                    
+                    requesting = false;
+                })
+                
+            }
 		}
 	}
   
@@ -41,4 +64,14 @@ function($scope, $http, $ionicLoading, $location, SvcNL) {
 	$scope.$on('$stateChangeSuccess', function() {
 		$scope.loadMore();
 	});
+    
+        // Filter
+    $scope.$watch('filterText', function() {
+        index = 1;
+        maxAuthors = 9999999;
+        $scope.authors = [];
+        $ionicScrollDelegate.scrollTop();
+        $scope.GetNextAuthors();
+    });
+
 }]);
--- www/js/abooks-titles.js
+++ www/js/abooks-titles.js
@@ -1,6 +1,6 @@
 // Audio books controller
-app.controller('ABooksTitlesCtrl', ['$scope', '$timeout', '$http', '$ionicLoading', 'SvcNL', 
-function($scope, $timeout, $http, $ionicLoading, SvcNL) {
+app.controller('ABooksTitlesCtrl', ['$scope', '$timeout', '$http', '$ionicLoading', '$ionicScrollDelegate', 'SvcNL', 
+function($scope, $timeout, $http, $ionicLoading, $ionicScrollDelegate, SvcNL) {
 
 	var index = 1;
 	var maxTitles = 9999999;
@@ -8,6 +8,7 @@ function($scope, $timeout, $http, $ionicLoading, SvcNL) {
 	var requesting = false;
 	
 	$scope.titles = [];
+    $scope.filterText = "";
 
 	$scope.GetNextTitles = function() {
 	
@@ -15,22 +16,42 @@ function($scope, $timeout, $http, $ionicLoading, SvcNL) {
 			
 			requesting = true;
 			
-			$http({
-				method: 'GET',
-				url: baseUrl + 'GetTitles?Session=' + SvcNL.GetSession() + '&Index=' + index + '&Count=' + pageSize
-			})
-			.then(function success(response) {
-			
-				maxTitles = response.data.GetTitlesResult.Total;
-				
-				response.data.GetTitlesResult.Titles.forEach(function(element) {
-					$scope.titles.push(element);
-				}, this);
-				
-				index += pageSize;
-				
-				requesting = false;
-			})
+            if ($scope.filterText=="") {
+                $http({
+                    method: 'GET',
+                    url: baseUrl + 'GetTitles?Session=' + SvcNL.GetSession() + '&Index=' + index + '&Count=' + pageSize
+                })
+                .then(function success(response) {
+                
+                    maxTitles = response.data.GetTitlesResult.Total;
+                    
+                    response.data.GetTitlesResult.Titles.forEach(function(element) {
+                        $scope.titles.push(element);
+                    }, this);
+                    
+                    index += pageSize;
+                    
+                    requesting = false;
+                })
+            }
+            else {
+                $http({
+                    method: 'GET',
+                    url: baseUrl + 'SearchTitles?Session=' + SvcNL.GetSession() + '&Text=' + $scope.filterText + '&Index=' + index + '&Count=' + pageSize
+                })
+                .then(function success(response) {
+                
+                    maxTitles = response.data.SearchTitlesResult.Total;
+                    
+                    response.data.SearchTitlesResult.Titles.forEach(function(element) {
+                        $scope.titles.push(element);                            
+                    }, this);
+                    
+                    index += pageSize;
+                    
+                    requesting = false;
+                })                
+            }
 		}
 	}
   
@@ -41,5 +62,14 @@ function($scope, $timeout, $http, $ionicLoading, SvcNL) {
   
 	$scope.$on('$stateChangeSuccess', function() {
 		$scope.loadMore();
-	});  
+	});
+    
+    // Filter
+    $scope.$watch('filterText', function() {
+        index = 1;
+        maxTitles = 9999999;
+        $scope.titles = [];
+        $ionicScrollDelegate.scrollTop();
+        $scope.GetNextTitles();
+    });
 }]);
